fc77f2c1ab9964a6a3357fe2503709957e7d62c8
ceduardo <cs.eduardo@icloud.com>
2022-01-22 09:10:09 -0300

    Solves mininum-swap-2

--- interview-preparation-kit/Arrays/minimum-swap-2.js
+++ interview-preparation-kit/Arrays/minimum-swap-2.js
@@ -1,19 +1,24 @@
 function minimumSwaps(arr) {
-    let result = 0
-    for (let i = 0; i < arr.length-1; i++) {
-        for (let j = i+1; j < arr.length-1; j++) {
-            const currNum = arr[i]
-            const nextNum = arr[j]
-            if(currNum > nextNum) {
-                console.log(`Swapping ${currNum} and ${nextNum}`)
-                arr[i] = nextNum
-                arr[j] = currNum
-                result++
-            }
-        }
+  let count = 0;
+  const entries = Object.entries(arr).map(([key,value]) => {
+    return [`${parseInt(key)+1}`, value];
+  });
+  const indexMap = Object.fromEntries(new Map(entries));
+  for(let i = 0; i < arr.length; i++) {
+    while(indexMap[`${i+1}`] != i+1) {
+      let temp = indexMap[`${indexMap[`${i+1}`]}`];
+      indexMap[`${indexMap[`${i+1}`]}`] = indexMap[`${i+1}`];
+      indexMap[`${i+1}`] = temp;
+      count++;
     }
-    return result
+  }
+  return count;
 }
 
-const result = minimumSwaps([1,3,5,2,4,6,7])
-console.log(`Result: ${result}`)
+function swap() {
+
+}
+
+let arr = [3, 7, 6, 9, 1, 8, 10, 4, 2, 5];
+let result = minimumSwaps(arr);
+console.log(`Result: ${result}`);
